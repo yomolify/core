ce51ccb57b8134e61cf848eaaf5883a857bf7830
yomolify <sidhu.amol@gmail.com>
2020-09-19 20:44:55 -0700

    [#174894373] exec_trade LS1

--- strategies/BollingerBands/LS1.py
+++ strategies/BollingerBands/LS1.py
@@ -27,9 +27,9 @@ class LS1(StrategyBase):
     )
 
     def __init__(self):
+        StrategyBase.__init__(self)
         self.position_bar = None
         self.position_highest = None
-
         self.bollinger_bands = bt.ind.BollingerBands(
             period=self.params.period_bb_sma, devfactor=self.params.period_bb_std, plot=False)
         self.sma_fast = bt.ind.SMA(
@@ -46,91 +46,23 @@ class LS1(StrategyBase):
             period=self.params.period_highest_high_mid, plot=False)
         self.highest_high_fast = bt.ind.Highest(
             period=self.params.period_highest_high_fast, plot=False)
-
         cross_down_bb_top = bt.ind.CrossDown(self.datas[0].close, self.bollinger_bands.lines.top)
         cross_down_bb_bot = bt.ind.CrossDown(self.datas[0].close, self.bollinger_bands.lines.bot)
-
         cross_up_bb_bot = bt.ind.CrossUp(self.datas[0].close, self.bollinger_bands.lines.bot)
-
         volSMA_slow = bt.ind.SMA(self.data.volume, subplot=True, period = self.params.period_vol_sma_slow, plot=False)
         volSMA_fast = bt.ind.SMA(self.data.volume, subplot=True, period = self.params.period_vol_sma_fast, plot=False)
-
         vol_condition = volSMA_fast > volSMA_slow
-
         self.bollinger_bands_width = (self.bollinger_bands.lines.top - self.bollinger_bands.lines.bot)/self.bollinger_bands.lines.mid
-
         vli_fast = bt.ind.SMA(self.bollinger_bands_width, subplot=True, period = self.params.period_bbw_sma_vli_fast, plot=False)
         vli_slow = bt.ind.SMA(self.bollinger_bands_width, subplot=True, period = self.params.period_bbw_sma_vli_slow, plot=False)
-        
         self.vli_top = vli_slow + 2*bt.ind.StdDev(self.bollinger_bands_width, period=self.params.period_bbw_sma_vli_slow)
         self.low_volatility_level = vli_fast < vli_slow
-  
         self.buy_sig = bt.And(cross_down_bb_top, vol_condition)
         self.close_sig = bt.And(cross_down_bb_bot, vol_condition)
-
         self.sell_sig = bt.And(cross_up_bb_bot, vol_condition)
-
         self.low = 0
 
-    def start(self):
-        self.val_start = self.broker.get_cash()
-
-    def stop(self):
-        # calculate the actual returns
-        self.roi = (self.broker.get_value() / self.val_start) - 1.0
-        print('\nROI:        {:.2f}%'.format(100.0 * self.roi))  
-
-    def next(self):
-        self.update_indicators()
-
-        # print('self.position_bar', self.position_bar)
-
-        if self.position_bar:
-            self.position_highest = max(self.position_highest, self.data.high[0])
-
-        # Check if an order is pending ... if yes, we cannot send a 2nd one
-        if self.order:
-            return
-
-        # Check if we are in the market
-        if not self.position:
-            if self.buy_sig:
-                if self.data0.close[0] > self.sma_fast[0]:
-                    if self.bollinger_bands_width < self.vli_top:
-                        if self.low_volatility_level:
-                            if self.sma_mid > self.sma_veryslow:
-                                self.low = self.data0.low[0]
-                                self.order = self.buy(exectype=self.params.exectype)
-                                self.buy_price_close = self.data0.close[0]
-                        else:
-                            self.low = self.data0.low[0]
-                            self.order = self.buy(exectype=self.params.exectype)
-                            self.buy_price_close = self.data0.close[0]
-                    elif self.sma_slow > self.sma_veryslow:
-                        self.low_alt = self.data0.low[-1]
-                        self.order = self.buy(exectype=self.params.exectype)
-                        self.buy_price_close = self.data0.close[0]
-
-                        if self.data.close[0] < self.data0.low[-1]:
-                            self.close(exectype=self.params.exectype)
-                        if self.profit_percentage > 3:
-                            self.sl_price = 1.01*self.buy_price_close
-            
-            elif self.sell_sig:
-                self.order = self.sell(exectype=self.params.exectype)
-                self.sell_price_close = self.data0.close[0]
-
-        if self.close_sig:
-            self.tp_price = self.data0.close[0]
-            self.close(exectype=self.params.exectype)
-        
-        if self.stop_loss:
-            self.stop_loss = False
-            self.close(exectype=self.params.exectype)
-
     def update_indicators(self):
-        # Position size is in BTC
-        # Profit is in USD
         self.profit = 0
         # LONG
         if self.position.size > 0:
@@ -160,3 +92,45 @@ class LS1(StrategyBase):
                 self.sl_price = self.highest_high_fast[0]
             if self.data.close[0] >= self.sl_price:
                 self.stop_loss = True
+
+    def next(self):
+        self.update_indicators()
+        if self.position_bar:
+            self.position_highest = max(self.position_highest, self.data.high[0])
+        if self.order:
+            return
+
+        if not self.position:
+            if self.buy_sig:
+                if self.data0.close[0] > self.sma_fast[0]:
+                    if self.bollinger_bands_width < self.vli_top:
+                        if self.low_volatility_level:
+                            if self.sma_mid > self.sma_veryslow:
+                                self.low = self.data0.low[0]
+                                self.order = self.exec_trade(direction="buy", exectype=self.params.exectype)
+                                self.buy_price_close = self.data0.close[0]
+                        else:
+                            self.low = self.data0.low[0]
+                            self.order = self.exec_trade(direction="buy", exectype=self.params.exectype)
+                            self.buy_price_close = self.data0.close[0]
+                    elif self.sma_slow > self.sma_veryslow:
+                        self.low_alt = self.data0.low[-1]
+                        self.order = self.exec_trade(direction="buy", exectype=self.params.exectype)
+                        self.buy_price_close = self.data0.close[0]
+
+                        if self.data.close[0] < self.data0.low[-1]:
+                            self.exec_trade(direction="close", exectype=self.params.exectype)
+                        if self.profit_percentage > 3:
+                            self.sl_price = 1.01*self.buy_price_close
+            
+            elif self.sell_sig:
+                self.order = self.sell(exectype=self.params.exectype)
+                self.sell_price_close = self.data0.close[0]
+
+        if self.close_sig:
+            self.tp_price = self.data0.close[0]
+            self.exec_trade(direction="close", exectype=self.params.exectype)
+        
+        if self.stop_loss:
+            self.stop_loss = False
+            self.exec_trade(direction="close", exectype=self.params.exectype)
