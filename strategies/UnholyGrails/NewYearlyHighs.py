9c8a9861cf28a73fd48178b6b5f94f41890cd169
Amol Sidhu <sidhu.amol@gmail.com>
2021-03-08 11:53:54 -0800

    supertrend + RSI entry

--- strategies/UnholyGrails/NewYearlyHighs.py
+++ strategies/UnholyGrails/NewYearlyHighs.py
@@ -26,10 +26,14 @@ class NewYearlyHighs(StrategyBase):
     def __init__(self):
         StrategyBase.__init__(self)
         self.i = 0
+        self.strategy = "NewYearlyHighs"
         self.bitcoin = self.datas[0]
         self.altcoins = self.datas
         self.inds = {}
         self.orders = dict()
+        self.entry_bar_height = {}
+        self.long_stop_order = {}
+
         self.bitcoin_atr = bt.indicators.AverageTrueRange(self.bitcoin, plot=False)
         self.bitcoin_sma = bt.indicators.SimpleMovingAverage(self.bitcoin.close-self.bitcoin_atr,
                                                             period=self.params.period_sma_bitcoin, plot=False)
@@ -40,9 +44,11 @@ class NewYearlyHighs(StrategyBase):
 
         for d in self.datas:
             ticker = d._name
+            self.entry_bar_height[ticker] = None
+            self.long_stop_order[ticker] = None
             self.inds[ticker] = {}
             self.inds[ticker]["rolling_high"] = bt.indicators.Highest(d.close, period=self.params.period_rolling_high, plot=False, subplot=False)
-            self.inds[ticker]["rolling_low"] = bt.indicators.Lowest(d.close, period=self.params.period_rolling_low, plot=True, subplot=False)
+            self.inds[ticker]["rolling_low"] = bt.indicators.Lowest(d.close, period=self.params.period_rolling_low, plot=False, subplot=False)
             self.inds[ticker]["average_true_range"] = bt.indicators.AverageTrueRange(d, plot=False)
             self.inds[ticker]["sma_veryfast"] = bt.ind.SimpleMovingAverage(d.close,
                 period=self.params.period_sma_veryfast, plot=False)
@@ -64,7 +70,25 @@ class NewYearlyHighs(StrategyBase):
             self.inds[ticker]["sma_adx_20"] = bt.ind.HMA(self.inds[ticker]["adx_20"], period=20, plot=True, subplot=True)
             self.inds[ticker]["roc"] = bt.ind.ROC(d, plot=True)
             self.inds[ticker]["sma_roc"] = bt.ind.HMA(self.inds[ticker]["roc"], plot=False)
-            self.inds[ticker]["super_trend"] = SuperTrend(d, plot=False, subplot=False)
+            self.inds[ticker]["super_trend"] = SuperTrend(d, plot=True, subplot=False)
+
+    # def next_open(self):
+    #     if (self.check_for_live_data and self.status == "LIVE") or not self.check_for_live_data:
+    #         available = list(filter(lambda d: len(d) > 500, self.altcoins))
+    #         available.sort(reverse=True, key=lambda d: (self.inds[d._name]["rsi"][0]) * (self.inds[d._name]["adx"][0]) * (self.inds[d._name]["roc"][0]))
+    #         for i, d in enumerate(available):
+    #             ticker = d._name
+    #             if "BTC" in ticker:
+    #                 print(f"{dt.datetime.now()} ----- Heartbeat Check OK -----")
+    #                 # if self.i % 20 == 0:
+    #                 #     self.rebalance_portfolio()
+    #                 # self.i += 1
+    #             current_position = self.get_position(d=d, attribute='size')
+    #             if current_position > 0:
+    #                 if self.long_stop_order[ticker]:
+    #                     self.cancel(self.long_stop_order[ticker])
+    #                     self.long_stop_order[ticker] = None
+    #                     self.long_stop_order[ticker] = self.sell(data=d, size=current_position, exectype=bt.Order.Stop, price=self.inds[ticker]["super_trend"][0])
 
     def next(self):
         if (self.check_for_live_data and self.status == "LIVE") or not self.check_for_live_data:
@@ -79,14 +103,21 @@ class NewYearlyHighs(StrategyBase):
                     self.i += 1
                 current_position = self.get_position(d=d, attribute='size')
                 if current_position > 0:
+                    # if self.long_stop_order[ticker]:
+                    #     self.cancel(self.long_stop_order[ticker])
+                    #     self.long_stop_order[ticker] = None
+                    #     self.long_stop_order[ticker] = self.sell(data=d, size=current_position, exectype=bt.Order.Stop, price=self.inds[ticker]["super_trend"][0])
                     if d.close[0] < self.inds[ticker]['rolling_low'][-1] or (self.inds[ticker]["sma_adx_20"][0] > 45 and self.inds[ticker]["sma_adx_20"][-1] > self.inds[ticker]["sma_adx_20"][0]):
                         try:
+                            # if self.long_stop_order[ticker]:
+                            #     self.cancel(self.long_stop_order[ticker])
                             order = self.add_order(data=d, target=0, type='market')
                         except Exception as e:
                             self.log("ERROR: {}".format(sys.exc_info()[0]))
                             self.log("{}".format(e))
                         print(f'{ticker} ADX SMA[-1]: {self.inds[ticker]["sma_adx_20"][-1]}, ADX SMA[0]: {self.inds[ticker]["sma_adx_20"][0]}')
-                elif current_position < 0:
+                if current_position < 0:
+                    self.log('NOT SUPPOSED TO BE HERE')
                     if (self.bitcoin.high[0] > self.bitcoin_sma[0]) or (d.high[0] > self.inds[ticker]['rolling_high'][-1]):
                         try:
                             order = self.add_order(data=d, target=0, type="market")
@@ -94,15 +125,12 @@ class NewYearlyHighs(StrategyBase):
                             self.log("ERROR: {}".format(sys.exc_info()[0]))
                             self.log("{}".format(e))
                 if current_position == 0:
+                    if self.long_stop_order[ticker]:
+                        self.cancel(self.long_stop_order[ticker])
                     volatility = self.inds[ticker]["average_true_range"][0]/d.close[0]
                     volatility_factor = 1/(volatility*100)
-                    closes_above_sma = 0
-                    for lookback in [0, -1, -2, -3, -4]:
-                        if d.close[lookback] > self.inds[ticker]['sma_veryslow'][lookback]:
-                            closes_above_sma += 1
-                    # if closes_above_sma == 5:
-                    #     if d.close[0] > self.inds[ticker]['sma_fast'][0]:
                     if (d.close[0] > self.inds[ticker]["sma_slow"] and d.close[0] > self.inds[ticker]["super_trend"][0]) or self.inds[ticker]["rsi"][0] < 20:
+                        self.entry_bar_height[ticker] = self.inds[ticker]["average_true_range"][0]
                         try:
                             self.orders[ticker] = [self.add_order(data=d, target=((self.p.order_target_percent/100) * volatility_factor), type="market")]
                         except Exception as e:
